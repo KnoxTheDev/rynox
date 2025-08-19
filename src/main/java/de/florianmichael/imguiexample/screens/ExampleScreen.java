62d58ff94c2513c8ad4230857a87f8b6bc26f01b
Enaium <32991121+Enaium@users.noreply.github.com>
2025-08-20 01:47:14 +0800

    Allow render interface to be implemented in screens; Fixup mc<->imgui render ordering (#21)
    Co-authored-by: FlorianMichael <florian.michael07@gmail.com>

--- src/main/java/de/florianmichael/imguiexample/screens/ExampleScreen.java
+++ src/main/java/de/florianmichael/imguiexample/screens/ExampleScreen.java
@@ -1,15 +1,13 @@
 package de.florianmichael.imguiexample.screens;
 
-import de.florianmichael.imguiexample.imgui.ImGuiImpl;
+import de.florianmichael.imguiexample.imgui.RenderInterface;
 import imgui.ImGui;
 import imgui.ImGuiIO;
-import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImBoolean;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
-public final class ExampleScreen extends Screen {
+public final class ExampleScreen extends Screen implements RenderInterface {
 
     private static final ImBoolean showDemoWindow = new ImBoolean(false);
 
@@ -18,17 +16,12 @@ public final class ExampleScreen extends Screen {
     }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        ImGuiImpl.draw(this::renderImGui);
-    }
-
-    private void renderImGui(final ImGuiIO io) {
-        if (ImGui.begin("Hello, World!", ImGuiWindowFlags.None)) {
+    public void render(ImGuiIO io) {
+        if (ImGui.begin("Hello, World!")) {
             ImGui.setWindowSize(800, 600);
-
             ImGui.checkbox("Show Demo Window", showDemoWindow);
+            ImGui.end();
         }
-        ImGui.end();
 
         ImGui.showDemoWindow(showDemoWindow);
     }
